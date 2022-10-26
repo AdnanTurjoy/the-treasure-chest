178b2bcd0e2fd459b2503af89775c7c922ca68e3
AdnanTurjoy <aturjoy4@gmail.com>
2022-10-26 22:33:51 +0600

    route solve

--- src/auth/Login/Login.js
+++ src/auth/Login/Login.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import { useState } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 
@@ -10,7 +10,7 @@ function Login(props) {
   const { signIn } = UserAuth();
 
   const location = useLocation();
-  const { from } = location.state || { from: { pathname: "/" } };
+  const { from } = location.state || { from: { pathname: "/the-treasure-chest" } };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,7 +26,6 @@ function Login(props) {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -69,12 +68,12 @@ function Login(props) {
                 type="submit"
                 className="w-full  font-bold text-white bg-[#0f3460] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Login 
+                Login
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 haven't an account?{" "}
                 <Link
-                  to="/signup"
+                  to="/the-treasure-chest/register"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Sign Up Here
--- src/auth/Register/Register.js
+++ src/auth/Register/Register.js
@@ -14,7 +14,7 @@ function SignUp() {
 
     try {
       await createUser(email, password);
-      history.push("/");
+      history.push("/the-treasure-chest");
     } catch (e) {
       console.log(e.message);
     }
@@ -97,7 +97,7 @@ function SignUp() {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
                 <Link
-                  to="/login"
+                  to="/the-treasure-chest/login"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Login Here
